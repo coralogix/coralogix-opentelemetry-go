a38d5b9ca58db86d0fd28c64597d22367762389e
ilya morgenshtern <ilya.m@coralogix.com>
2024-11-27 14:14:55 +0200

    start flow when consumer/server

--- sampler/coralogix-sampler_test.go
+++ sampler/coralogix-sampler_test.go
@@ -308,4 +308,123 @@ func TestCoralogixSampler_ShouldSample(t *testing.T) {
 		assert.ElementsMatch(t, expectedAttributes, result.Attributes)
 		assert.Equal(t, expectedTraceState, result.Tracestate)
 	})
+
+	t.Run("When_SetFlow_ParentContextExistsAndNotRemote_ShouldCopyParentTraceState", func(t *testing.T) {
+		alwaysSampler := traceSdk.AlwaysSample()
+		coralogixSampler := NewCoralogixSampler(alwaysSampler)
+
+		traceState := traceCore.TraceState{}
+
+		traceState, _ = traceState.Insert(TransactionIdentifierTraceState, "fatherSpanName")
+		traceState, _ = traceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		parentSpan := traceCore.NewSpanContext(traceCore.SpanContextConfig{
+			TraceID:    traceCore.TraceID{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
+			SpanID:     traceCore.SpanID{0xFF, 0xFE, 0xFD, 0xFC, 0xFB, 0xFA, 0xF9, 0xF8},
+			TraceFlags: traceCore.FlagsSampled,
+			TraceState: traceState,
+			Remote:     false,
+		})
+		parentCtx := traceCore.ContextWithSpanContext(context.Background(), parentSpan)
+
+		// Act
+		parameters := traceSdk.SamplingParameters{
+			ParentContext: parentCtx,
+			Name:          spanName,
+			Attributes:    []attribute.KeyValue{},
+		}
+		result := coralogixSampler.ShouldSample(parameters)
+
+		expectedAttributes := []attribute.KeyValue{
+			attribute.String(TransactionIdentifier, "fatherSpanName"),
+			attribute.String(DistributedTransactionIdentifier, "fatherSpanName"),
+		}
+		expectedTraceState := traceCore.TraceState{}
+		expectedTraceState, _ = traceState.Insert(TransactionIdentifierTraceState, "fatherSpanName")
+		expectedTraceState, _ = traceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		assert.ElementsMatch(t, expectedAttributes, result.Attributes)
+		assert.Equal(t, expectedTraceState, result.Tracestate)
+	})
+
+	t.Run("When_ParentContextExistsAndNotRemoteButServer_ShouldCopyNotParentTraceState", func(t *testing.T) {
+		alwaysSampler := traceSdk.AlwaysSample()
+		coralogixSampler := NewCoralogixSampler(alwaysSampler)
+
+		traceState := traceCore.TraceState{}
+
+		traceState, _ = traceState.Insert(TransactionIdentifierTraceState, "fatherSpanName")
+		traceState, _ = traceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		parentSpan := traceCore.NewSpanContext(traceCore.SpanContextConfig{
+			TraceID:    traceCore.TraceID{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
+			SpanID:     traceCore.SpanID{0xFF, 0xFE, 0xFD, 0xFC, 0xFB, 0xFA, 0xF9, 0xF8},
+			TraceFlags: traceCore.FlagsSampled,
+			TraceState: traceState,
+			Remote:     false,
+		})
+		parentCtx := traceCore.ContextWithSpanContext(context.Background(), parentSpan)
+
+		// Act
+		parameters := traceSdk.SamplingParameters{
+			ParentContext: parentCtx,
+			Name:          spanName,
+			Attributes:    []attribute.KeyValue{},
+			Kind:          traceCore.SpanKindServer,
+		}
+		result := coralogixSampler.ShouldSample(parameters)
+
+		expectedAttributes := []attribute.KeyValue{
+			attribute.String(TransactionIdentifier, "spanName"),
+			attribute.String(DistributedTransactionIdentifier, "fatherSpanName"),
+			attribute.Bool(TransactionIdentifierRoot, true),
+		}
+		expectedTraceState := traceCore.TraceState{}
+		expectedTraceState, _ = expectedTraceState.Insert(TransactionIdentifierTraceState, spanName)
+		expectedTraceState, _ = expectedTraceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		assert.ElementsMatch(t, expectedAttributes, result.Attributes)
+		assert.Equal(t, expectedTraceState.Get(TransactionIdentifierTraceState), result.Tracestate.Get(TransactionIdentifierTraceState))
+		assert.Equal(t, expectedTraceState.Get(DistributedTransactionIdentifierTraceState), result.Tracestate.Get(DistributedTransactionIdentifierTraceState))
+	})
+	t.Run("When_ParentContextExistsAndNotRemoteButServer_ShouldCopyNotParentTraceState", func(t *testing.T) {
+		alwaysSampler := traceSdk.AlwaysSample()
+		coralogixSampler := NewCoralogixSampler(alwaysSampler)
+
+		traceState := traceCore.TraceState{}
+
+		traceState, _ = traceState.Insert(TransactionIdentifierTraceState, "fatherSpanName")
+		traceState, _ = traceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		parentSpan := traceCore.NewSpanContext(traceCore.SpanContextConfig{
+			TraceID:    traceCore.TraceID{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
+			SpanID:     traceCore.SpanID{0xFF, 0xFE, 0xFD, 0xFC, 0xFB, 0xFA, 0xF9, 0xF8},
+			TraceFlags: traceCore.FlagsSampled,
+			TraceState: traceState,
+			Remote:     false,
+		})
+		parentCtx := traceCore.ContextWithSpanContext(context.Background(), parentSpan)
+
+		// Act
+		parameters := traceSdk.SamplingParameters{
+			ParentContext: parentCtx,
+			Name:          spanName,
+			Attributes:    []attribute.KeyValue{},
+			Kind:          traceCore.SpanKindConsumer,
+		}
+		result := coralogixSampler.ShouldSample(parameters)
+
+		expectedAttributes := []attribute.KeyValue{
+			attribute.String(TransactionIdentifier, "spanName"),
+			attribute.String(DistributedTransactionIdentifier, "fatherSpanName"),
+			attribute.Bool(TransactionIdentifierRoot, true),
+		}
+		expectedTraceState := traceCore.TraceState{}
+		expectedTraceState, _ = expectedTraceState.Insert(TransactionIdentifierTraceState, spanName)
+		expectedTraceState, _ = expectedTraceState.Insert(DistributedTransactionIdentifierTraceState, "fatherSpanName")
+
+		assert.ElementsMatch(t, expectedAttributes, result.Attributes)
+		assert.Equal(t, expectedTraceState.Get(TransactionIdentifierTraceState), result.Tracestate.Get(TransactionIdentifierTraceState))
+		assert.Equal(t, expectedTraceState.Get(DistributedTransactionIdentifierTraceState), result.Tracestate.Get(DistributedTransactionIdentifierTraceState))
+	})
 }
